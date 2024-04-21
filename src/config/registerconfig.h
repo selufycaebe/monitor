a9f60dfaec1b2f4add5c3d9df639e6f9a4806df1
githqk <hqk_mail@qq.com>
2024-04-21 18:28:46 +0800

    init v0.1.3 - 修改文件名大小写问题

---
+++ src/config/registerconfig.h
@@ -0,0 +1,16 @@
+#ifndef REGISTERCONFIG_H
+#define REGISTERCONFIG_H
+
+#include <QMetaType>
+
+#include "devices/exam1.h"
+#include "devices/common.h"
+namespace cfg {
+inline void ClassRegisterInit() {
+    qRegisterMetaType<Exam1>();
+    qRegisterMetaType<Common>();
+    //...
+}
+}
+
+#endif // REGISTERCONFIG_H
